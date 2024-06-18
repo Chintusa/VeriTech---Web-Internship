473cfa06b19a2bebc2ed13f4cba9a14f439d6c33
Jhasaketan Sa <jhasketansa86@gmail.com>
2024-06-18 13:40:08 +0530

    Update main.js

--- Day-4/main.js
+++ Day-4/main.js
@@ -2,31 +2,31 @@ let movies = [
     {
       name: "falcon and the winter soldier",
       des:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        "Following the events of “Avengers: Endgame,” Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
       image: "images/slider 2.png"
     },
     {
       name: "loki",
       des:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
       image: "images/slider 1.png"
     },
     {
       name: "wanda vision",
       des:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        "Wanda Maximoff and Vision—two super-powered beings living idealized suburban lives—begin to suspect that everything is not as it seems.",
       image: "images/slider 3.png"
     },
     {
       name: "raya and the last dragon",
       des:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        "Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.",
       image: "images/slider 4.png"
     },
     {
       name: "luca",
       des:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+        "The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.",
       image: "images/slider 5.png"
     }
   ];
